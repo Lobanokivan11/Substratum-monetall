2cff4251818c68c6b5c988ab332a967cc2979634
Lobanokivan11 <lobanokivan11@gmail.com>
2024-12-18 20:48:38 +0300

    @

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+    kotlin {
+            jvmToolchain(17)
+    }
     sourceSets {
         named("main") {
             java.srcDir("src/main/kotlin")

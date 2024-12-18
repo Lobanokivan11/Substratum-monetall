cd8e525d1077d65b0d5c972b4246ea34696372a3
Lobanokivan11 <lobanokivan11@gmail.com>
2024-12-18 19:00:27 +0300

    F

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ buildscript {
     dependencies {
         classpath("com.android.tools.build:gradle:7.4.2")
         classpath(kotlin("gradle-plugin", version = Constants.kotlinVersion))
-        implementation("org.jetbrains.kotlin:kotlin-stdlib")
+        api("org.jetbrains.kotlin:kotlin-stdlib")
     }
 }
 
